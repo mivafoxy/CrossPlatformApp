17cbee5bc74adef594d36c7694530cd6dc8e16ce
malakhoviv <malakhoviv@osbank.ru>
2023-04-30 16:14:52 +0300

    Initial Commit

---
+++ CrossPlatformApp/CrossPlatformAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossPlatformAppApp.swift
+//  CrossPlatformApp
+//
+//  Created by Илья Малахов on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrossPlatformAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
